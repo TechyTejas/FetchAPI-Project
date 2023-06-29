f3468da452414818da22cbf4985aa6841e68a121
tejas <sadadetej.27@gmail.com>
2023-06-29 13:42:07 +0530

    Fetch API introduction

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
+//we have used swapi 
+import React,{useState} from 'react';
+
+import MoviesList from './components/MoviesList';
 import './App.css';
 
 function App() {
+  const[movies,setMovies]=useState([])
+
+ function fetchMovieHandler() {
+  fetch('https://swapi.dev/api/films/')
+  .then((response)=>{
+    return response.json();
+  })
+  .then((data) => {
+    //this step is to transformed upcoming data into that keyformat in which we alloted
+    const trasformedMovies = data.results.map(movieData => {
+      return {
+        id: movieData.episode_id,
+        title: movieData.title,
+        openingText: movieData.opening_crawl,
+        releaseDate: movieData.release_date 
+      };
+    });
+   setMovies(trasformedMovies);
+  })
+ }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button onClick={fetchMovieHandler}>Fetch Movies</button>
+      </section>
+      <section>
+        <MoviesList movies={movies} />
+      </section>
+    </React.Fragment>
   );
 }
 
